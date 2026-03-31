9c2d0357ae53a220aca776e2d15f385184f78087
tahatakgungor <tahatakgungor@gmail.com>
2026-03-31 22:58:45 +0300

    style(login): remove background illustration shapes from login and register pages

--- harri-front-end/src/components/login-register/login-area.js
+++ harri-front-end/src/components/login-register/login-area.js
@@ -1,7 +1,6 @@
 'use client';
 import Link from "next/link";
 // internal
-import Shapes from "./shapes";
 import LoginForm from "@components/forms/login-form";
 import { useLanguage } from "src/context/LanguageContext";
 
@@ -11,7 +10,6 @@ const LoginArea = () => {
     <section className="login__area pt-110 pb-110">
       <div className="container">
         <div className="login__inner p-relative z-index-1">
-          <Shapes/>
           <div className="row justify-content-center">
             <div className="col-xl-6 col-lg-8 col-md-10">
               <div className="login__wrapper">
